4982d7780fbeda00b60c22963add34cf3bd860da
Kapil Chaudhary <chaudharikapil16@gmail.com>
2022-01-21 05:28:04 +0530

    MealsToGo -  cleanup code

--- 4__MealsToGo_Major_Project/MealsToGo/src/features/restaurants/components/restaurantInfo.js
+++ 4__MealsToGo_Major_Project/MealsToGo/src/features/restaurants/components/restaurantInfo.js
@@ -1,58 +1,25 @@
 import React from "react";
-import { Text, Image, View } from "react-native";
+import { Text } from "react-native";
 
 import { Spacer } from "../../../components/spacer/spacerComponent";
 
-import { Card } from 'react-native-paper';
-import styled from "styled-components";
+import { 
+  RestaurantCard,
+
+  RestaurantCardCover,
+  Info,
+  Section,
+  SectionEnd,
+  Rating,
+  
+  Address,
+  Icon,
+} from "./restaurantInfoStyles";
 
 import { SvgXml } from "react-native-svg";
 
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
-
-const RestaurantCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-  padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const Rating = styled.View`
-  flex-direction: row;
-  padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
-`;
-
-const Section = styled.View`
-  flex-direction: row;
-  align-items: center;
-`;
-
-const SectionEnd = styled.View`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-end;
-`;
-
-// Create a <Title> react-native component that renders an Text component which is
-// red and sized at 16px
-const Title = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body};
-  color: ${(props) => props.theme.colors.ui.primary};
-`;
-
-const Address = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.caption};
-`;
-
-const Info = styled.View`
-  padding: ${(props) => props.theme.space[3]};
-`;
 
                     // set restaurant to be an empty object for now
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -84,7 +51,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
 
             <Info>
 
-              <Title> {name} </Title>
+              <Text variant="label"> {name} </Text>
 
               <Section>
 
@@ -97,7 +64,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
                 <SectionEnd>
 
                   { isClosedTemporarily && (
-                    <Text variant="label" style={{ color: "red" }}>
+                    <Text variant="error" style={{ color: "red" }}>
                       CLOSED TEMPORARILY
                     </Text>
                   ) }
@@ -110,8 +77,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
 
                   <Spacer variant="left.large"/>
                   
-                  <Image 
-                    style={{ width: 15, height: 15 }} 
+                  <Icon 
                     source={{ uri: icon }} 
                   />
 
